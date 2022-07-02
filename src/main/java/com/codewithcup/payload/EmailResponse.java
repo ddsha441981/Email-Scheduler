0c709ad74ea6f501c402e547918d594ed13a1a31
ddsha441981 <44318598+ddsha441981@users.noreply.github.com>
2022-07-02 15:45:26 +0530

    Quartz Scheduler

---
+++ src/main/java/com/codewithcup/payload/EmailResponse.java
@@ -0,0 +1,33 @@
+package com.codewithcup.payload;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class EmailResponse {
+
+    private boolean success;
+    private String jobId;
+    private String jobGroup;
+    private String message;
+
+
+    public EmailResponse(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public EmailResponse(boolean success, String jobId, String jobGroup, String message) {
+        this.success = success;
+        this.jobId = jobId;
+        this.jobGroup = jobGroup;
+        this.message = message;
+    }
+}
